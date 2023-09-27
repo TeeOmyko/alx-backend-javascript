fc52eb3c2b32ec47bca3d3e3779112b19aadc92e
TeeOmyko <amanwachiteresa@gmail.com>
2023-09-27 10:32:08 +0000

    ES6 promises

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,15 @@
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve(
+    {
+      firstName,
+      lastName,
+    },
+  );
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve(
+    {
+      firstName,
+      lastName,
+    },
+  );
+}}
